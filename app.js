9fe087d34143b2628bb5e6953cebc546a0115a56
Cagla <caglaeyubog@gmail.com>
2021-11-13 22:57:51 +0300

    mobile menu icon margin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function skillAnim() {
 function mobileMenu() {
     const largeMenu = navMenu.innerHTML;
     const menuIcon = `<a href="#welcome-section"><i class="far fa-star ms-4 fa-2x text-danger"></i></a>
-    <a href="#" class="ms-auto me-2"><i class="fas fa-bars text-light fa-2x"></i></a>`;
+    <a href="#mobile-menu" class="ms-auto me-4"><i class="fas fa-bars text-light fa-2x"></i></a>`;
 
     //mediaQuery.matches && (navMenu.innerHTML = menuIcon) ;
     (window.innerWidth < 575) ? (navMenu.innerHTML = menuIcon) : (navMenu.innerHTML = largeMenu);
